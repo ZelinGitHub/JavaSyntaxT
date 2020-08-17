1ea548d2ddcea38cd36de95608d257852ff268b3
ZelinGitHub <2414217563@qq.com>
2020-08-17 19:01:48 +0800

    firstCommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 
+import com.company.concur.InterruptTest;
 import com.company.concur.PoolTest;
 import com.company.concur.ThreadTest;
 import com.company.designmode.proxy.ProxyTest;
@@ -8,7 +9,7 @@ import com.company.designmode.proxy.ProxyTest;
 public class Main {
 
     public static void main(String[] args) {
-        ThreadTest.test();
+        InterruptTest.test();
     }
 
 
--- src/com/company/concur/ThreadTest.java
+++ src/com/company/concur/ThreadTest.java
@@ -5,9 +5,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class ThreadTest {
-    public static void test() {
-        testInterrupt();
-    }
+
 
     private static void testFutureTask() {
         MyCallable callable = new MyCallable();
@@ -20,35 +18,7 @@ public class ThreadTest {
         thread.start();
     }
 
-    private static void testInterrupt() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("线程开始执行");
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println("线程执行结束");
-                    }
-                });
-                thread.start();
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("中断前");
-                thread.interrupt();
-                System.out.println("中断后");
-            }
-        }).start();
 
-    }
 
     public static void testJoin() {
         Thread thread1 = new Thread(new Runnable() {
