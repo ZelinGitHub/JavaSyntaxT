410d8315d7ce3c8383ea8fe98c4ef747436a89b6
ZelinGithub <1628747456@qq.com>
2020-09-12 23:53:32 +0800

    Merge branch 'master' of https://github.com/ZelinGitHub/JavaSyntaxT
     Conflicts:
    	out/production/JavaSyntaxT/com/company/Main.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$1$1.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$1.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$2.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$3.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$4.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest.class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 
+import com.company.collect.CompareTest;
 import com.company.concur.spinlock.SpinLockTest;
 
 public class Main {
 
     public static void main(String[] args) {
-        SpinLockTest.testCLHLock();
+        CompareTest.test2();
     }
 
 
