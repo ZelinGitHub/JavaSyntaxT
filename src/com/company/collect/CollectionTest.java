567b1587bb9375ec8a4fea29c57531ff139fba95
ZelinGithub <1628747456@qq.com>
2020-09-19 14:36:19 +0800

    Merge branch 'master' of https://github.com/ZelinGitHub/JavaSyntaxT
     Conflicts:
    	out/production/JavaSyntaxT/com/company/Main.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$1$1.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$1.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$2.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$3.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest$4.class
    	out/production/JavaSyntaxT/com/company/concur/ThreadTest.class

--- src/com/company/collect/CollectionTest.java
+++ src/com/company/collect/CollectionTest.java
@@ -5,21 +5,8 @@ import java.util.*;
 public class CollectionTest {
 
     private static void testCollection() {
-        Collection<String> collection;
-        Queue<String> queue;
-        Set<String> set = new HashSet<>();
-        Set<String> set2 = new TreeSet<>();
-        PriorityQueue<String> priorityQueue;
-
-        Deque<String> deque = new LinkedList<>();
-        List<String> list = new LinkedList<>();
-        List<String> list2 = new ArrayList<>();
-        list.remove(0);
-        Iterator<String> iterator = list.iterator();
-        Iterator<String> iterator2 = deque.iterator();
-        Iterator<String> iterator1 = list.listIterator();
-
-        ArrayDeque arrayDeque;
+        Collection collection=new ArrayList();
+        Collection collection2=new LinkedList();
     }
 
 }
