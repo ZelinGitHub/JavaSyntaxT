eeae25a5e618fc5a34e5fb60ccea01416e3aad67
ZelinGitHub <2414217563@qq.com>
2020-12-15 19:34:01 +0800

    Exception throw finally

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ import com.company.java.concur.big3.Big3AtomicLongTest;
 import com.company.java.concur.big3.Big3SeeTest;
 import com.company.java.concur.big3.Big3SeeYieldTest;
 import com.company.java.concur.lock.other.SemaphoreTest;
+import com.company.java.exception.ExceptionTest;
 
 import static com.company.kotlin.lambda.LambdaTestKt.testLambdaTest2;
 
 public class Main {
 
     public static void main(String[] args) {
-        testLambdaTest2();
+        ExceptionTest.test10();
     }
 
 }
