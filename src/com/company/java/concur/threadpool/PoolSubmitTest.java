f05904658f59df9d0ebcff973d4453c99748ab85
ZelinGitHub <2414217563@qq.com>
2020-12-16 19:02:32 +0800

    ThreadPool

--- src/com/company/java/concur/threadpool/PoolSubmitTest.java
+++ src/com/company/java/concur/threadpool/PoolSubmitTest.java
@@ -1,34 +1,83 @@
 package com.company.java.concur.threadpool;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class PoolSubmitTest {
-    public static void test(){
-        ExecutorService threadPool = Executors.newCachedThreadPool();
 
-        threadPool.submit(new Runnable() {
+
+    public static void test1() {
+
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1
+                , 1
+                , 0
+                , TimeUnit.SECONDS
+                , linkedBlockingQueue
+        );
+        System.out.println("主线程：向线程池提交任务");
+        Future<?> futureTask = threadPoolExecutor.submit(new Runnable() {
             @Override
             public void run() {
-                System.out.println("我是提交给线程池的任务");
+                long tId = Thread.currentThread().getId();
+                System.out.println("线程" + tId + "：正在执行任务");
             }
         });
+        try {
+            System.out.println("主线程：得到任务结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException pE) {
+            pE.printStackTrace();
+        }
+    }
 
-        String result = "我是任务结果";
-        threadPool.submit(new Runnable() {
+    public static void test2() {
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1
+                , 1
+                , 0
+                , TimeUnit.SECONDS
+                , linkedBlockingQueue
+        );
+        System.out.println("主线程：向线程池提交任务");
+        String result = "糊弄完了";
+        Future<?> futureTask = threadPoolExecutor.submit(new Runnable() {
             @Override
             public void run() {
-                System.out.println("我是提交给线程池的任务");
+                long tId = Thread.currentThread().getId();
+                System.out.println("线程" + tId + "：正在执行任务");
             }
         }, result);
+        try {
+            System.out.println("主线程：得到任务结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException pE) {
+            pE.printStackTrace();
+        }
+    }
 
-        threadPool.submit(new Callable<String>() {
+    public static void test3() {
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1
+                , 1
+                , 0
+                , TimeUnit.SECONDS
+                , linkedBlockingQueue
+        );
+        System.out.println("主线程：向线程池提交任务");
+        Future<?> futureTask = threadPoolExecutor.submit(new Callable<String>() {
             @Override
             public String call() throws Exception {
-                return "我是提交给线程池的任务结果";
+                long tId = Thread.currentThread().getId();
+                System.out.println("线程" + tId + "：正在执行任务");
+                return "糊弄完了";
             }
         });
-
+        try {
+            System.out.println("主线程：得到任务结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException pE) {
+            pE.printStackTrace();
+        }
     }
+
 }
