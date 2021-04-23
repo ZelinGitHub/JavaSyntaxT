55852726f3ce4dc007ebe1e6a36663172cdfb11e
ZelinGitHub <2414217563@qq.com>
2021-04-23 18:58:30 +0800

    kotlin

--- src/com/company/java/concur/threadpool/PoolCreateTest.java
+++ src/com/company/java/concur/threadpool/PoolCreateTest.java
@@ -3,7 +3,24 @@ package com.company.java.concur.threadpool;
 import java.util.concurrent.*;
 
 public class PoolCreateTest {
-    public static void test(){
+    public void createPoolOrigin() {
+        //创建阻塞队列
+        //类型参数是Runnable
+        BlockingQueue<Runnable> linkedBlockingQueue
+                = new LinkedBlockingQueue<Runnable>(
+                20
+        );
+        //创建ThreadPoolExecutor实例（线程池）
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                10
+                , 15
+                , 30
+                , TimeUnit.SECONDS
+                , linkedBlockingQueue
+        );
+    }
+
+    public void createPoolOrigin2() {
         BlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<Runnable>(20);
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 10
@@ -13,11 +30,15 @@ public class PoolCreateTest {
                 , linkedBlockingQueue
         );
         threadPoolExecutor.allowCoreThreadTimeOut(true);
-
     }
 
-    public static void test2(){
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(4);
+
+    public void createPoolByExecutors() {
+        //创建缓存线程池
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        //创建单一线程池
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        //创建固定线程池
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(4);
     }
 }
