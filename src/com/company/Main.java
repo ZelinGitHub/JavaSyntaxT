2e1835d8e18a25179278535320711ebf4d086613
ZelinGitHub <2414217563@qq.com>
2021-04-27 18:30:45 +0800

    kotlin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,13 @@ package com.company;
 
 import com.company.kotlin.collection.FilterMapTestKt;
 
+import static com.company.java.concur.thread.ThreadExceptionTest.testThreadException;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        testThreadException();
     }
 
 }
--- src/com/company/java/concur/thread/ThreadExceptionTest.java
+++ src/com/company/java/concur/thread/ThreadExceptionTest.java
@@ -1,7 +1,35 @@
 package com.company.java.concur.thread;
 
 public class ThreadExceptionTest {
-    public static void test1() {
+    public static void testThreadException() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long threadId = Thread.currentThread().getId();
+                System.out.println("线程" + threadId + "：开始执行");
+                Thread thread1 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        long threadId = Thread.currentThread().getId();
+                        System.out.println("线程" + threadId + "：开始执行");
+                        System.out.println("线程" + threadId + "：准备发生异常");
+                        int a = 3 / 0;
+                        System.out.println("线程" + threadId + "：执行结束");
+                    }
+                });
+                System.out.println("线程" + threadId + "：开启新线程");
+                thread1.start();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException pE) {
+                    pE.printStackTrace();
+                }
+                System.out.println("线程" + threadId + "：执行结束");
+            }
+        }).start();
+    }
+
+    public static void testUncaughtExceptionHandler() {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -21,7 +49,7 @@ public class ThreadExceptionTest {
         thread1.start();
     }
 
-    public static void test2() {
+    public static void testDefaultUncaughtExceptionHandler() {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -50,7 +78,7 @@ public class ThreadExceptionTest {
         thread2.start();
     }
 
-    public static void test3() {
+    public static void testDefaultAndNotDefaultUncaughtExceptionHandler() {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -66,7 +94,7 @@ public class ThreadExceptionTest {
                 long threadId = Thread.currentThread().getId();
                 System.out.println("线程" + threadId + "：线程" + t.getId()
                         + "发生异常" + e.getLocalizedMessage()
-                        +"（默认异常处理器）"
+                        + "（默认异常处理器）"
                 );
             }
         });
@@ -77,7 +105,7 @@ public class ThreadExceptionTest {
                 long threadId = Thread.currentThread().getId();
                 System.out.println("线程" + threadId + "：线程" + t.getId()
                         + "发生异常" + e.getLocalizedMessage()
-                        +"（单独的异常处理器）"
+                        + "（单独的异常处理器）"
                 );
             }
         });
